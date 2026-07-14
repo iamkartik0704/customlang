342a9912b7ba43bfb47edf7780726ad5a90c716d
iamkartik0704 <iamkartik0704@gmail.com>
2026-07-14 12:37:48 +0530

    AST added

---
+++ ast.ts
@@ -0,0 +1,35 @@
+// this works as the grammar for the language
+
+
+// TypeScript String Literal Union.
+export type NodeType = "Program" | "BinaryExp" | "NumericLiteral" | "Identifier";
+// we can aslo include functionDeclaration,unaryExpression,callExpression
+
+export interface Stmt{
+    kind:NodeType;
+}
+export interface Program extends Stmt{
+    kind:"Program";
+    body:Stmt[]
+    // body expects an array of Stmt, it is polymorphic. It means it can hold absolutely anything that extends Stmt
+}
+
+export interface Expr extends Stmt{}
+
+export interface BinaryExp extends Expr{
+    kind:"BinaryExp";
+    left:Expr;
+    right:Expr;
+    operator:string;
+}
+
+export interface NumericLiteral extends Expr{
+    kind:"NumericLiteral";
+    value:number;
+}
+
+export interface Identifier extends Expr
+{
+    kind:"Identifier";
+    symbol:string;
+}
